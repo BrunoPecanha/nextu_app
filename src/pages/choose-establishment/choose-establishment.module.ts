ca65652df57b804bfbf251404cd90108c6c7175b
Bruno Peçanha <brunoitdesenv@gmail.com>
2025-04-28 01:43:20 -0300

    💄 Adiciona componente de rodapé a todas as páginas que usam

--- src/pages/choose-establishment/choose-establishment.module.ts
+++ src/pages/choose-establishment/choose-establishment.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { ChooseEstablishmentPageRoutingModule } from './choose-establishment-routing.module';
 
 import { ChooseEstablishmentPage } from './choose-establishment.page';
+import { SharedModule } from 'src/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     ChooseEstablishmentPageRoutingModule
   ],
   declarations: [ChooseEstablishmentPage]
--- src/pages/queue/queue.module.ts
+++ src/pages/queue/queue.module.ts
@@ -7,11 +7,13 @@ import { IonicModule } from '@ionic/angular';
 import { QueuePageRoutingModule } from './queue-routing.module';
 
 import { QueuePage } from './queue.page';
+import { SharedModule } from 'src/shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    SharedModule,
     IonicModule,
     QueuePageRoutingModule
   ],
